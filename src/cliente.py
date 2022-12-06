59d619fdd05171e6220a10a5e7b23b9dd0f2825b
Karolaine Lima <92931508+kartulle@users.noreply.github.com>
2022-12-05 23:47:24 -0300

    Update cliente.py

--- src/cliente.py
+++ src/cliente.py
@@ -6,7 +6,7 @@ import time
 
 PORT = 5050
 FORMATO = 'utf-8'
-SERVER = "192.168.0.105"
+SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
